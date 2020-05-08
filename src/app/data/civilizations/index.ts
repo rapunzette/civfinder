516de26d33b27398055e0f08f9e14b3f9aa1b1e5
evheath <elliot.v.heath@gmail.com>
2020-05-08 14:00:57 -0500

    byzantines

--- src/app/data/civilizations/index.ts
+++ src/app/data/civilizations/index.ts
@@ -2,9 +2,11 @@ import { Civilization } from 'src/app/models/civilization.model';
 import { aztecs } from './aztecs';
 import { berbers } from './berbers';
 import { britons } from './britons';
+import { byzantines } from './byzantines';
 
 export const civilizations: Civilization[] = [
   aztecs,
   berbers,
   britons,
+  byzantines,
 ]
