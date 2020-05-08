aa2e7275e9fa3f6c1037e39500d0a18e35cbaae4
evheath <elliot.v.heath@gmail.com>
2020-05-08 11:19:39 -0500

    technology generator

---
+++ customSchematics/src/technology/index.ts
@@ -0,0 +1,22 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function technology(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const { name, age, color } = _options;
+    let underscoreName = name.split(" ").join("_")
+    let filepath = `/src/app/data/technologies/${underscoreName}.ts`
+    let code = `
+import { Technology } from 'src/app/models/technology.model'
+export const ${underscoreName}: Technology = {
+  "name": "${name}",
+  "age": "${age}",
+  "color": "${color}",
+}
+`
+
+    tree.create(filepath, code);
+    return tree;
+  };
+}
