85a8445b1b5938057d7e05630e032517c6c5032a
evheath <elliot.v.heath@gmail.com>
2020-05-10 09:38:09 -0500

    excluded civilizations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,38 @@ import { Civilization } from './models/civilization.model';
 import { civilizations } from './data/civilizations';
 import { technologies } from './data/technologies';
 
+import { difference } from 'lodash';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
+  /**
+   * A list of all technologies.
+   * 
+   * This does not include unique techs for now
+   */
   public techs: Technology[] = technologies;
+  /**
+   * The technologies the user cares about
+   */
   public selectedTechs: string[] = [];
+  /**
+ * Civilizations that have the selected technologies
+ */
   public correspondingCivs: Civilization[];
+  /**
+   * Civilizations that do NOT have the selected technologies
+   */
   public excludedCivs: Civilization[];
 
+  /**
+   * Ran everytime a new technology is selected or unselected
+   * @param tech the technology in question
+   * @param nowChecked the status of the technology
+   */
   public onChange(tech: Technology, nowChecked: boolean): void {
 
     if (nowChecked) {
@@ -26,22 +47,27 @@ export class AppComponent implements OnInit, OnDestroy {
       this.selectedTechs.splice(i, 1);
     }
 
-    this.updateCorrespondingCivs();
+    this.updateCivs();
   }
 
-  private updateCorrespondingCivs(): void {
+  /**
+   * Updates correspondingCivs and excludedCivs depending on the selected techs
+   */
+  private updateCivs(): void {
 
     this.correspondingCivs = civilizations.filter(civ => {
       return this.selectedTechs.every(tech => {
         return civ[tech];
       });
-    })
+    });
+
+    this.excludedCivs = difference(civilizations, this.correspondingCivs);
   }
 
   constructor() {
   }
   ngOnInit(): void {
-    this.updateCorrespondingCivs();
+    this.updateCivs();
   }
   ngOnDestroy() {
   }
