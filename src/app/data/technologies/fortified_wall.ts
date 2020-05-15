03606af3f6f408b04dee3710ece89f6f9bac0d78
evheath <elliot.v.heath@gmail.com>
2020-05-15 13:14:50 -0500

    some dependency fixes

--- src/app/data/technologies/fortified_wall.ts
+++ src/app/data/technologies/fortified_wall.ts
@@ -1,8 +1,9 @@
 
 import { Technology } from 'src/app/models/technology.model'
 import { university } from './university'
+import { stone_wall } from './stone_wall'
 export const fortified_wall: Technology = {
-  dependencies: [university.name,],
+  dependencies: [university.name, stone_wall.name],
   "name": "fortified wall",
   "age": "castle",
   "color": "red",
--- src/app/data/technologies/guard_tower.ts
+++ src/app/data/technologies/guard_tower.ts
@@ -1,8 +1,9 @@
 
 import { Technology } from 'src/app/models/technology.model'
 import { university } from './university'
+import { watch_tower } from './watch_tower'
 export const guard_tower: Technology = {
-  dependencies: [university.name,],
+  dependencies: [university.name, watch_tower.name],
   "name": "guard tower",
   "age": "castle",
   "color": "red",
