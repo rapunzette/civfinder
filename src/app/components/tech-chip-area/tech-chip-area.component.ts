ddf7bbe69b2abd5a9d80c738afd15d7dd8552c27
evheath <elliot.v.heath@gmail.com>
2020-05-16 14:37:31 -0500

    building the graph from selected techs

--- src/app/components/tech-chip-area/tech-chip-area.component.ts
+++ src/app/components/tech-chip-area/tech-chip-area.component.ts
@@ -11,41 +11,18 @@ import { TechService } from 'src/app/tech.service';
   templateUrl: './tech-chip-area.component.html',
   styleUrls: ['./tech-chip-area.component.scss']
 })
-export class TechChipAreaComponent {
-  visible = true;
-  selectable = true;
+export class TechChipAreaComponent implements OnInit {
   techCtrl = new FormControl();
   filteredTechs: Observable<Technology[]>;
-  /**
-   * The techs selected by the user
-   * 
-   * These will appear as chips
-   * 
-   * Note: this list is entirely local to this component. Meaning the entire tech dependency tree/graph will not show up here
-   */
-  techs: Technology[] = [halberdier];
-
   @ViewChild('techInput') techInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
-  constructor(public techService: TechService) {
+  constructor(public techService: TechService) { }
+
+  ngOnInit(): void {
     this.filteredTechs = this.techCtrl.valueChanges.pipe(
       startWith(null),
       map((usrInput: string | Technology | null) => usrInput ? this._filter(usrInput) : technologies));
-  }
-
-  /**
-   * Called when the user clicks the 'X' next to a chip
-   * @param tech The tech to be removed
-   */
-  public remove(tech: Technology): void {
-    const index = this.techs.indexOf(tech);
-
-    if (index >= 0) {
-      this.techs.splice(index, 1);
-    }
-
-    this.techService.remove(tech);
   }
 
   /**
@@ -55,10 +32,7 @@ export class TechChipAreaComponent {
   public selected(event: MatAutocompleteSelectedEvent): void {
     let newTech: Technology = event.option.value;
     // add the tech only if it hasn't already been selected
-    if (this.techs.indexOf(newTech) == -1) {
-      this.techs.push(newTech);
-      this.techService.add(newTech);
-    }
+    this.techService.select(newTech);
     this.techInput.nativeElement.value = '';
     this.techCtrl.setValue(null);
   }
