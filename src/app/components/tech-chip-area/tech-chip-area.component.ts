b151548c621a386a9b371676ac9d7c0083475fbb
evheath <elliot.v.heath@gmail.com>
2020-05-16 13:14:00 -0500

    incorporating the tech service

--- src/app/components/tech-chip-area/tech-chip-area.component.ts
+++ src/app/components/tech-chip-area/tech-chip-area.component.ts
@@ -3,9 +3,8 @@ import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { startWith, map } from 'rxjs/operators';
-import { MatChipInputEvent } from '@angular/material/chips';
 import { Technology } from 'src/app/models/technology.model';
-import { technologies } from 'src/app/data/technologies';
+import { technologies, halberdier } from 'src/app/data/technologies';
 import { TechService } from 'src/app/tech.service';
 @Component({
   selector: 'app-tech-chip-area',
@@ -19,8 +18,12 @@ export class TechChipAreaComponent {
   filteredTechs: Observable<Technology[]>;
   /**
    * The techs selected by the user
+   * 
+   * These will appear as chips
+   * 
+   * Note: this list is entirely local to this component. Meaning the entire tech dependency tree/graph will not show up here
    */
-  techs: Technology[] = [];
+  techs: Technology[] = [halberdier];
 
   @ViewChild('techInput') techInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
@@ -31,19 +34,31 @@ export class TechChipAreaComponent {
       map((usrInput: string | Technology | null) => usrInput ? this._filter(usrInput) : technologies));
   }
 
-  remove(tech: Technology): void {
+  /**
+   * Called when the user clicks the 'X' next to a chip
+   * @param tech The tech to be removed
+   */
+  public remove(tech: Technology): void {
     const index = this.techs.indexOf(tech);
 
     if (index >= 0) {
       this.techs.splice(index, 1);
     }
+
+    this.techService.remove(tech);
   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
+  /**
+   * called when a user selects a tech from the drop down chip input
+   * 
+   */
+  public selected(event: MatAutocompleteSelectedEvent): void {
     let newTech: Technology = event.option.value;
     this.techs.push(newTech);
     this.techInput.nativeElement.value = '';
     this.techCtrl.setValue(null);
+
+    this.techService.add(newTech);
   }
 
 
